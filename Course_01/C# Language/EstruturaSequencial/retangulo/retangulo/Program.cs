bcdd83d624b9d5bc04deaf2537dfae2399840eb3
Voznak <c410.1981@gmail.com>
2022-02-02 09:26:58 -0300

    EstruturaSequencialC#

---
+++ Course_01/C# Language/EstruturaSequencial/retangulo/retangulo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Globalization;
+
+namespace Retangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            double dbase, altura, area, perimetro, diagonal;
+
+            Console.Write("Base do retangulo: ");
+            dbase = double.Parse(Console.ReadLine(), ci);
+            Console.Write("Altura do retangulo: ");
+            altura = double.Parse(Console.ReadLine(), ci);
+
+            area = dbase * altura;
+            perimetro = (2 * dbase) + (2 * altura);
+            diagonal = Math.Sqrt(Math.Pow(dbase, 2.0) + Math.Pow(altura, 2.0));
+
+            Console.WriteLine("AREA = " + area.ToString("F4",ci));
+            Console.WriteLine("PERIMETRO = " + perimetro.ToString("F4", ci));
+            Console.WriteLine("DIAGONAL = " + diagonal.ToString("F4", ci));
+
+        }
+    }
+}
---
+++ Course_01/C# Language/EstruturaSequencial/terreno/terreno/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Globalization;
+
+namespace Terreno
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            double largura, comprimento, valor, area, precoTotal;
+
+            Console.Write("Digite a largura do terreno: ");
+            largura = double.Parse(Console.ReadLine(), ci);
+            Console.Write("Digite o comprimento do terreno: ");
+            comprimento = double.Parse(Console.ReadLine(),ci);
+            Console.Write("Digite o valor do metro quadrado: ");
+            valor = double.Parse(Console.ReadLine(), ci);
+
+            area = largura * comprimento;
+            precoTotal = valor * area;
+
+            Console.WriteLine("Área do terreno = " + area.ToString("F2", ci) + " m^2");
+            Console.WriteLine("Preço do terreno = R$" + precoTotal.ToString("F2", ci));
+        }
+    }
+}
