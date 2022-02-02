f05bf200a8180844d1d662083515b15a16826510
Voznak <c410.1981@gmail.com>
2022-02-01 21:11:07 -0300

    C# start

---
+++ Course_01/C# Language/Start/ProcDeDadosCasting/ProcDeDadosCasting/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Globalization;
+
+namespace ProcDeDadosCasting
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            int x, y;
+
+            x = 10;
+            y = 20;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            int q;
+            double w;
+
+            q = 5;
+            w = 2 * q;
+
+            Console.WriteLine(q);
+            Console.WriteLine(w.ToString("F2", CI));
+
+            double b1, b2, h, area;
+            b1 = 6;
+            b2 = 8;
+            h = 5;
+            area = (b1 + b2) / 2 * h;
+            Console.WriteLine(area.ToString("F2", CI));
+
+            int a, b;
+            double resultado;
+            a = 5;
+            b = 2;
+            resultado = (double) a / b;
+            Console.WriteLine(resultado);
+
+            double c;
+            int d;
+            c = 5.0;
+            d = (int) a;
+
+
+
+        }
+    }
+}
---
+++ Course_01/C# Language/Start/SaidaDeDados/SaidaDeDados/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Globalization;
+
+namespace SaidaDeDados
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture; //Para criar uma formaa reduzida que permita formatar o uso de ponto no lugar da virgula
+            
+            Console.WriteLine("Olá Mundo!");
+
+            Console.Write("Bom dia!");
+            Console.Write("Boa noite!");
+
+            int x, y;
+            x = 10;
+            y = 20;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            double w;
+            w = 2.3456;
+            Console.WriteLine(w.ToString("F2", CultureInfo.InvariantCulture)); //para mudar somente numa variavel
+            Console.WriteLine(w.ToString("F2", CI));
+
+            int idade;
+            double salario;
+            string name;
+            char sexo;
+
+            idade = 32;
+            salario = 4560.9;
+            name = "Maria Silva";
+            sexo = 'F';
+
+            Console.WriteLine("A funcionária " + name + ", sexo " + sexo + ", ganha R$"
+                + salario.ToString("F2", CI) + " e tem " + idade + " anos.");
+        }
+    }
+}
+
+
