8cdad490340710d9e5466a0c2472a6bfd59a3d46
Voznak <c410.1981@gmail.com>
2022-02-05 07:05:19 -0300

    EstruturaCondicionalC#

---
+++ Course_01/C# Language/EstruturaCondicional/dardo/dardo/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Globalization;
+
+namespace Dardo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            double a, b, c, maior;
+
+            Console.WriteLine("Digite as três distâncias: ");
+            a = double.Parse(Console.ReadLine(), ci);
+            b = double.Parse(Console.ReadLine(), ci);
+            c = double.Parse(Console.ReadLine(), ci);
+
+            maior = a;
+
+            if (maior < b)
+            {
+                maior = b;
+            }
+            else if (maior < c)
+            {
+                maior = c;
+            }
+
+            Console.WriteLine("MAIOR DISTÂNCIA = " + maior.ToString("F2", ci));
+        }
+    }
+}
---
+++ Course_01/C# Language/EstruturaCondicional/menorDeTres/menorDeTres/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace MenorDeTres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo ci = CultureInfo.InvariantCulture;
+
+            int a, b, c, menor;
+
+            Console.Write("Primeiro valor: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Segundo valor: ");
+            b = int.Parse(Console.ReadLine());
+            Console.Write("Terceiro valor: ");
+            c = int.Parse(Console.ReadLine());
+
+            menor = a;
+
+            if (menor > b)
+            {
+                menor = b;
+            }
+            else if (menor > c)
+            {
+                menor = c;
+            }
+
+            Console.WriteLine("MENOR = " + menor);
+        }
+    }
+}
